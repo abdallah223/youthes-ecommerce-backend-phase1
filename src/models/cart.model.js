0f5c3ff1a6b3da4f7aecae61d3932e58d7d82857
Abdallah Omar <abdallahomar417@gmail.com>
2026-03-14 19:46:41 +0200

    feat: add cart model

---
+++ src/models/cart.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const CartItemSchema = new mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [1, "Quantity must be at least 1"],
+    },
+    priceAtAdd: { type: Number, required: true, min: 0 },
+  },
+  { _id: false },
+);
+
+const CartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    items: [CartItemSchema],
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const Cart = mongoose.model("Cart", CartSchema);
